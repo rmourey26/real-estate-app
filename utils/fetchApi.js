3b570fa5bc6dc07a85c0f571b6421e0e4bb2f279
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-05-22 22:59:23 -0400

    Update fetchApi.js

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 // Base URL
-export const baseURL = "https://real-estate-app3.p.rapidapi.com";
+export const baseURL = "https://bayut.p.rapidapi.com";
 
 // Fetch Data from API
 export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
       "X-RapidAPI-Key": process.env.NEXT_APP_RAPID_API_KEY,
-      "X-RapidAPI-Host": "real-estate-app3.p.rapidapi.com",
+      "X-RapidAPI-Host": "bayut.p.rapidapi.com",
     },
   });
 

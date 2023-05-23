b3abc5d8eb12f4ee01e6695257f63e2cad70c8ef
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-05-22 23:00:13 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["bayut-production.s3.eu-central-1.amazonaws.com"],
+    domains: ["bayut-production.s3.eu-central-1.amazonaws.com", "bayut.p.rapidapi.com"],
   },
 };
 
